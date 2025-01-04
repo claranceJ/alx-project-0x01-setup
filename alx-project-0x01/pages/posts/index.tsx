0f7c71b03e03bcf9fe18ca32eb2277f93756ecf9
claranceJ <jacktonec@gmail.com>
2025-01-04 12:54:40 +0300

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,5 +1,15 @@
+import Header from "@/components/layout/Header";
+
 const Posts: React.FC = () => {
-    return <div>Posts Page</div>;
-  };
-  
+  return (
+    <div>
+      <Header />
+      <main className="container mx-auto py-6">
+        <h1 className="text-3xl font-bold">Posts</h1>
+        <p>List of posts will go here.</p>
+      </main>
+    </div>
+  );
+}
+
 export default Posts;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,15 @@
+import Header from "@/components/layout/Header";
+
 const Users: React.FC = () => {
-    return <div>Users Page</div>;
-  };
-  
+  return (
+    <div>
+      <Header />
+      <main className="container mx-auto py-6">
+        <h1 className="text-3xl font-bold">Users</h1>
+        <p>List of users will go here.</p>
+      </main>
+    </div>
+  );
+}
+
 export default Users;
